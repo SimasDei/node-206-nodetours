1c08b96d532e9b93d3243c5c12a85ccb274001e9
SimasDei <simondeiman@gmail.com>
2019-10-09 19:51:08 +0300

    update controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,67 +1,92 @@
 const fs = require('fs');
 
-/**
- * @resource - Tour data
- */
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkId = (req, res, next, val) => {
-  console.log(req.params.id);
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      success: false,
-      msg: 'invalid Id'
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
     });
   }
   next();
 };
 
 exports.getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, results: tours.length, data: { tours } });
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 };
+
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+};
+
 exports.createTour = (req, res) => {
+  // console.log(req.body);
+
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
+
   fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
+    `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
-      res
-        .status(201)
-        .json({ success: true, results: tours.length, data: { tours } });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
     }
   );
 };
-exports.getTour = (req, res) => {
-  const { id } = req.params;
-  if (id > tours.length) {
-    return res.status(404).json({ success: false, msg: 'No tour found' });
-  }
 
-  const tour = tours.find(tour => tour.id === parseInt(id, 10));
-  res.status(200).json({ success: true, data: { tour } });
-};
 exports.updateTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find(tour => tour.id === parseInt(id, 10));
-
-  if (tour) {
-    return res.status(200).json({ success: true, data: { tour } });
-  } else
-    return res.status(404).json({ success: false, msg: 'No such tour found' });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
 };
-exports.deleteTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find(tour => tour.id === parseInt(id, 10));
 
-  if (tour) {
-    return res.status(200).json({ success: true, data: null });
-  } else
-    return res.status(404).json({ success: false, msg: 'No such tour found' });
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,30 @@
-const fs = require('fs');
-
-/**
- * @resource - Users data
- */
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
-);
-
 exports.getAllUsers = (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, results: users.length, data: { users } });
-};
-exports.createUser = (req, res) => {
-  const newId = users[users.length - 1].id + 1;
-  const newUser = Object.assign({ id: newId }, req.body);
-
-  users.push(newUser);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/users.json`,
-    JSON.stringify(users),
-    err => {
-      res
-        .status(201)
-        .json({ success: true, results: users.length, data: { users } });
-    }
-  );
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
 };
 exports.getUser = (req, res) => {
-  const { id } = req.params;
-  if (id > users.length) {
-    return res.status(404).json({ success: false, msg: 'No user found' });
-  }
-
-  const user = users.find(user => user._id === id);
-  res.status(200).json({ success: true, data: { user } });
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
 };
 exports.updateUser = (req, res) => {
-  const { id } = req.params;
-  const user = users.find(user => user._id === id);
-
-  if (user) {
-    return res.status(200).json({ success: true, data: { user } });
-  } else
-    return res.status(404).json({ success: false, msg: 'No such user found' });
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
 };
 exports.deleteUser = (req, res) => {
-  const { id } = req.params;
-  const user = users.find(user => user._id === id);
-
-  if (user) {
-    return res.status(200).json({ success: true, data: null });
-  } else
-    return res.status(404).json({ success: false, msg: 'No such user found' });
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
 };
