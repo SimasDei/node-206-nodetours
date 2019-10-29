9d24ba46af6c7e69523cf6c2052944308cbd91f3
Simas <simonas.deimantavicius@mediapark.com>
2019-10-29 14:10:59 +0200

    test insertion of new Tours

--- server.js
+++ server.js
@@ -36,6 +36,16 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(error => console.log({ error, msg: 'Oh No! 🎃 🎢 🎈.' }));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
