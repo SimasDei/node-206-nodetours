75815081eb70612c7c4b471bc63f8b5958142a66
Simas <simas@simas-dei.com>
2020-03-03 19:24:44 +0200

    add bcrypt and password hashing for singup endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: 'success',
     data: {
-      user: newUser
-    }
+      user: newUser,
+    },
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,8 +29,18 @@ const userSchema = new mongoose.Schema({
       validator: function(el) {
         return el === this.password;
       },
+      message: 'Passwords do not match',
     },
   },
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
