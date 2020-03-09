fc5acbbde41a2a2203f9a2e912b171ed43a5fc7c
Simas <simas@simas-dei.com>
2020-03-09 17:47:46 +0200

    add pre find mongo query middleware

--- models/userModel.js
+++ models/userModel.js
@@ -64,6 +64,11 @@ userSchema.pre('save', function(next) {
   next();
 });
 
+userSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
 userSchema.methods.correctPassword = async function(candidate, userPassword) {
   return await bcrypt.compare(candidate, userPassword);
 };
