a714c3f5897b62850143411e38b258ee7651d953
Simas <simas@simas-dei.com>
2020-03-09 17:25:37 +0200

    add update current user endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -31,6 +34,31 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates 😭, please use the correct API endpoint',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
