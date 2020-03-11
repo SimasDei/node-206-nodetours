18c04a1d8ee47d6dc86f124de2deb1be579a810a
Simas <simas@simas-dei.com>
2020-03-11 15:42:56 +0200

    implement data modeling

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -17,7 +17,7 @@ mongoose
   })
   .then(() => console.log('Connected to the Database 🎉 ✨'));
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
