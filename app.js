b115d96a572d0697374b97f73519925f54735724
Simas <simas@simas-dei.com>
2020-03-14 19:31:35 +0200

    test get all and create review routes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHanlder = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -57,6 +58,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Route: ${req.originalUrl} was not found 😢`, 404));
