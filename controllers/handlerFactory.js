90a717b78f4cc9f8855f7bd5a8f9d8ef8ce2fc49
Simas <simas@simas-dei.com>
2020-03-17 16:44:29 +0200

    add updateOne factory function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,18 +1,6 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-exports.deleteOne = Model =>
-  catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const document = await Model.findByIdAndDelete(id);
-    if (!document) return next(new AppError('No such document found', 404));
-
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  });
-
 exports.updateOne = Model =>
   catchAsync(async (req, res, next) => {
     const { id } = req.params;
@@ -26,5 +14,28 @@ exports.updateOne = Model =>
       data: {
         data: document,
       },
+    });
+  });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: document,
+      },
+    });
+  });
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const document = await Model.findByIdAndDelete(id);
+    if (!document) return next(new AppError('No such document found', 404));
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
     });
   });
--- controllers/userController.js
+++ controllers/userController.js
@@ -29,13 +29,8 @@ exports.getUser = (req, res) => {
     message: 'This route, is not yet defined!',
   });
 };
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route, is not yet defined!',
-  });
-};
 
+exports.createUser = factory.createOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
 
