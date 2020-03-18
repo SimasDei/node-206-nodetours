3a5c0e6bf1bb7bee151a34c7af075c085c741382
Simas <simas@simas-dei.com>
2020-03-18 17:05:10 +0200

    add Reviews and Users to populate Database script

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -18,10 +20,14 @@ mongoose
   .then(() => console.log('Connected to the Database 🎉 ✨'));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
     console.log('Data successfully imported');
   } catch (error) {
     console.log(error);
@@ -32,6 +38,8 @@ const importData = async () => {
 const dropCollection = async () => {
   try {
     await Tour.deleteMany();
+    await Review.deleteMany();
+    await User.deleteMany();
     console.log('Data successfully deleted');
   } catch (error) {
     console.log(error);
