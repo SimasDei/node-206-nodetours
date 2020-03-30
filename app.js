dd68f5642d192304df07627bcf0493f574095603
Simas <simas@simas-dei.com>
2020-03-30 20:31:11 +0300

    refactor views routes into router and controller

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHanlder = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -60,12 +61,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res, next) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Sai',
-  });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
